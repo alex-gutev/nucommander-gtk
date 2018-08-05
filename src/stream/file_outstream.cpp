b9ae31677900ac6f73bafa2fdd1a6f17b88ede76
Alexander Gutev <alex.gutev@gmail.com>
2018-08-05 22:05:01 +0200

    Implement output stream interface for regular files.

---
+++ src/stream/file_outstream.cpp
@@ -0,0 +1,71 @@
+/*
+ * NuCommander
+ * Copyright (C) 2018  Alexander Gutev <alex.gutev@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "file_outstream.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+using namespace nuc;
+
+
+file_outstream::file_outstream(const char *path, int flags) {
+    if ((fd = open(path, flags | O_CLOEXEC | O_CREAT)) < 0) {
+        throw error(errno);
+    }
+}
+file_outstream::file_outstream(int dirfd, const char *path, int flags) {
+    if ((fd = open(path, flags | O_CLOEXEC | O_CREAT)) < 0) {
+        throw error(errno);
+    }
+}
+
+bool file_outstream::close() {
+    if (fd >= 0) return !::close(fd);
+
+    return true;
+}
+
+void file_outstream::write(const byte *buf, size_t n, off_t offset) {
+    seek(offset);
+
+    while(n) {
+        ssize_t bytes_written = ::write(fd, (const char *)buf, n);
+
+        if (bytes_written < 0) throw error(errno);
+
+        n -= bytes_written;
+        buf += bytes_written;
+    };
+}
+
+void file_outstream::seek(off_t offset) {
+    // Seeking past EOF results in "gaps" in the file which are filled
+    // with zeroes, provided that data is written past the
+    // gap. Currently it is assumed that if offset is non-zero then
+    // the size of the block is non-zero.
+
+    if (offset) {
+        if (lseek(fd, offset, SEEK_CUR))
+            throw error(errno);
+    }
+}
---
+++ src/stream/file_outstream.h
@@ -0,0 +1,85 @@
+/*
+ * NuCommander
+ * Copyright (C) 2018  Alexander Gutev <alex.gutev@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef NUC_FILE_OUTSTREAM_H
+#define NUC_FILE_OUTSTREAM_H
+
+#include "outstream.h"
+
+namespace nuc {
+    /**
+     * Regular File Output Stream.
+     */
+    class file_outstream : public outstream {
+        /**
+         * File descriptor.
+         */
+        int fd;
+
+        /**
+         * Seeks to the position @a offset bytes from the current
+         * position in the file.
+         *
+         * @param offset The offset, to seek to, from the current file
+         *   position.
+         */
+        void seek(off_t offset);
+
+    public:
+        /**
+         * Constructs a file output stream for the file at @a path.
+         *
+         * @param path Path to the file.
+         * @param flags Additional flags passed to open(2).
+         */
+        file_outstream(const char *path, int flags);
+        /**
+         * Constructs a file output stream for the file at path @a
+         * path relative to the directory with file descriptor @a
+         * dirfd.
+         *
+         * @param dirfd File descriptor of the directory.
+         *
+         * @param path Path to the file, if relative interpreted as
+         *    relative to the directory with file descriptor dirfd.
+         *
+         * @param flags Additional flags passed to openat(2).
+         */
+        file_outstream(int dirfd, const char *path, int flags);
+
+        /**
+         * Closes the output stream.
+         */
+        ~file_outstream() {
+            close();
+        }
+
+        /* Method overrides */
+
+        virtual bool close();
+
+        virtual void write(const byte *buf, size_t n, off_t offset = -1);
+    };
+}
+
+#endif
+
+// Local Variables:
+// mode: c++
+// End:
