0da5e79760e2aeb408bb05043000627897ba2f82
Alexander Gutev <alex.gutev@gmail.com>
2018-09-02 08:09:14 +0200

    Implement output stream interface for archives.

---
+++ src/stream/archive_outstream.cpp
@@ -0,0 +1,29 @@
+/*
+ * NuCommander
+ * Copyright (C) 2018  Alexander Gutev <alex.gutev@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "archive_outstream.h"
+
+
+using namespace nuc;
+
+void archive_outstream::write(const byte *buf, size_t n, off_t off) {
+    if (plugin->pack(handle, (const char *)buf, n, off)) {
+        raise_error(errno);
+    }
+}
---
+++ src/stream/archive_outstream.h
@@ -0,0 +1,76 @@
+/*
+ * NuCommander
+ * Copyright (C) 2018  Alexander Gutev <alex.gutev@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef NUC_ARCHIVE_OUTSTREAM_H
+#define NUC_ARCHIVE_OUTSTREAM_H
+
+#include "outstream.h"
+#include "plugins/archive_plugin.h"
+
+namespace nuc {
+    /**
+     * Archive Output Stream.
+     *
+     * Writes to files contained in archives.
+     */
+    class archive_outstream : public outstream {
+        /**
+         * Plugin for writing to the archive.
+         */
+        archive_plugin *plugin;
+
+        /**
+         * Handle of the open archive.
+         */
+        void *handle;
+
+    public:
+        /**
+         * Creates an archive outstream.
+         *
+         * No operations should be performed on the archive, and the
+         * handle should not be closed, while this stream is in use.
+         *
+         * @param plugin Plugin for writing to the archive.
+         * @param handle Handle of the open archive.
+         */
+        archive_outstream(archive_plugin *plugin, void *handle) : plugin(plugin), handle(handle) {}
+
+        ~archive_outstream() {
+            close();
+        }
+
+
+        /* Method Overrides */
+
+        virtual bool close() {
+            // Does nothing as it is the responsibility of the creator
+            // of this object to close the handle.
+            return true;
+        }
+
+        virtual void write(const byte *buf, size_t n, off_t offset);
+    };
+}
+
+#endif
+
+// Local Variables:
+// mode: c++
+// End:
