9dc887d502603890e2f77363c6bd5c6d754a41c1
Alexander Gutev <alex.gutev@gmail.com>
2018-10-20 09:38:03 +0200

    Integrate command framework, operation queue and error handling into app_window.

--- src/app_window.cpp
+++ src/app_window.cpp
@@ -20,44 +20,60 @@
 #include "app_window.h"
 
 #include <exception>
+#include <functional>
+#include <iostream>
+
+#include "tasks/async_task.h"
+
+#include "commands/commands.h"
+
+#include "copy/copy.h"
 
 using namespace nuc;
 
 app_window* app_window::create() {
     auto builder = Gtk::Builder::create_from_resource("/org/agware/nucommander/window.glade");
-    
+
     app_window *window = nullptr;
-    
+
     builder->get_widget_derived("commander_window", window);
-    
+
     if (!window)
         throw std::runtime_error("No \"commander_window\" object in window.glade");
-    
+
     return window;
 }
 
-app_window::app_window(Gtk::ApplicationWindow::BaseObjectType* cobject, 
+app_window::app_window(Gtk::ApplicationWindow::BaseObjectType* cobject,
                        const Glib::RefPtr<Gtk::Builder> &builder)
     : Gtk::ApplicationWindow(cobject), builder(builder) {
-    
+
     // TODO: Add error checking
-    
+
     set_default_size(800, 600);
-        
+
     builder->get_widget("pane_view", pane_view);
 
     init_pane_view();
 }
 
+app_window::~app_window() {
+    if (err_dialog)
+        delete err_dialog;
+}
+
 void app_window::init_pane_view() {
     set_focus_chain({pane_view});
-    
+
     add_file_view(left_view, 1);
     add_file_view(right_view, 2);
-    
+
+    left_view->next_file_view = right_view;
+    right_view->next_file_view = left_view;
+
     left_view->path("/");
     right_view->path("/");
-    
+
     pane_view->set_focus_chain({left_view, right_view});
     pane_view->show_all();
 }
@@ -65,20 +81,75 @@ void app_window::init_pane_view() {
 
 void app_window::add_file_view(nuc::file_view* & ptr, int pane) {
     auto builder = Gtk::Builder::create_from_resource("/org/agware/nucommander/fileview.glade");
-    
+
     // TODO: Add error checking
-    
+
     builder->get_widget_derived("file_view", ptr);
-    
+
     if (pane == 1) {
         pane_view->pack1(*ptr, true, true);
     }
     else {
         pane_view->pack2(*ptr, true, true);
     }
+
+    ptr->add_events(Gdk::KEY_RELEASE_MASK);
+    ptr->signal_key_release_event().connect(sigc::bind<file_view*>(sigc::mem_fun(this, &app_window::on_keypress), ptr), false);
 }
 
 
 Glib::RefPtr<Gtk::Builder> app_window::file_view_builder() {
     return Gtk::Builder::create_from_resource("/org/agware/nucommander/fileview.glade");
+}
+
+bool app_window::on_keypress(const GdkEventKey *e, file_view *src) {
+    using namespace std::placeholders;
+
+    std::string command_name = command_keymap::instance().command_name(e);
+
+    auto command = commands.find(command_name);
+
+    if (command != commands.end()) {
+        command->second(this, src);
+        return true;
+    }
+
+    return false;
+};
+
+void app_window::add_operation(task_queue::task_type op) {
+    using namespace std::placeholders;
+
+    operations->add(with_error_handler(std::move(op), std::bind(&app_window::handle_error, this, _1)));
+}
+
+void app_window::handle_error(const error &e) {
+    auto &actions = restarts();
+
+    std::promise<const restart *> promise;
+
+    dispatch_main([&] {
+        show_error(promise, e, actions);
+    });
+
+    const restart &r = *promise.get_future().get();
+    r(e);
+}
+
+
+/// Error Dialog
+
+void app_window::create_error_dialog() {
+    if (!err_dialog) {
+        err_dialog = error_dialog::create();
+
+        err_dialog->set_transient_for(*this);
+    }
+}
+
+void app_window::show_error(std::promise<const restart *> &promise, const nuc::error &err, const restart_map &restarts) {
+    create_error_dialog();
+
+    err_dialog->show(promise, err, restarts);
+    err_dialog->present();
 }
--- src/app_window.h
+++ src/app_window.h
@@ -31,6 +31,10 @@
 #include "cleanup_n.h"
 #include "file_view.h"
 
+#include "tasks/task_queue.h"
+
+#include "errors/error_dialog.h"
+
 namespace nuc {
     /**
      * Main Application Window.
@@ -59,6 +63,20 @@ namespace nuc {
          */
         file_view *right_view;
 
+
+        /**
+         * Error dialog: displays error details and a list of recovery
+         * options.
+         */
+        error_dialog *err_dialog = nullptr;
+
+
+        /**
+         * Operation task queue, onto which file operations are
+         * queued.
+         */
+        std::shared_ptr<task_queue> operations{task_queue::create()};
+
         /**
          * Creates and adds the file view widgets to the paned view
          * container, and sets up the focus chain.
@@ -81,17 +99,69 @@ namespace nuc {
          * Creates a new file view widget builder.
          */
         Glib::RefPtr<Gtk::Builder> file_view_builder();
-      
-    public:
+
         /**
-         * Constructor.
+         * Signal handler for the key press event on each file_view.
+         *
+         * @param e The event.
+         * @param src The file_view in which the event was triggered.
          */
+        bool on_keypress(const GdkEventKey *e, file_view *src);
+
+
+        /* Error handling */
+
+        /**
+         * File operation error handler.
+         *
+         * Displays a dialog with a list of error recovery options
+         * (restarts).
+         *
+         * @param e The error.
+         */
+        void handle_error(const error &e);
+
+
+        /* Error Dialog */
+
+        /**
+         * Creates the error dialog, and stores a pointer to it in
+         * err_dialog, if it has not been created already.
+         */
+        void create_error_dialog();
+
+        /**
+         * Displays the error dialog with a particular error and
+         * recovery options.
+         *
+         * @param promise A promise which is set to the value of the
+         *   chosen recovery option, once it is chosen by the user.
+         *
+         * @param err The error.
+         *
+         * @param restarts The restart map (recovery options).
+         */
+        void show_error(std::promise<const restart *> &promise, const error &err, const restart_map &restarts);
+
+
+    public:
+        /* Constructor. */
         app_window(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder> &builder);
+
+        /* Destructor */
+        ~app_window();
 
         /**
          * Creates a new application window object.
          */
         static app_window *create();
+
+        /**
+         * Adds an operation to the operations task queue.
+         *
+         * @param op The operation to add.
+         */
+        void add_operation(task_queue::task_type op);
 
         /**
          * Asynchronous cleanup method.
@@ -110,7 +180,7 @@ namespace nuc {
 template <typename F>
 void nuc::app_window::cleanup(F fn) {
     auto cleanup_fn = cleanup_n_fn(2, fn);
-    
+
     left_view->cleanup(cleanup_fn);
     right_view->cleanup(cleanup_fn);
 }
@@ -120,4 +190,3 @@ void nuc::app_window::cleanup(F fn) {
 // Local Variables:
 // mode: c++
 // End:
-
