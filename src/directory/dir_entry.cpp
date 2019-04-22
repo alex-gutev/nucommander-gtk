a7ca4bd278be7f96e4084900e5b3557f1b7cd898
Alexander Gutev <alex.gutev@gmail.com>
2019-04-22 17:08:03 +0200

    Clean up code in dir_entry class.

--- src/directory/dir_entry.cpp
+++ src/directory/dir_entry.cpp
@@ -24,7 +24,66 @@
 using namespace nuc;
 
 
-dir_entry::entry_type dir_entry::dt_to_entry_type(uint8_t type) {
+dir_entry::dir_entry(const pathname orig_name, uint8_t type) : dir_entry(orig_name, dt_to_entry_type(type)) {}
+
+// The default constructor call m_attr() is required to value
+// initialize all members of the stat struct to zero
+
+dir_entry::dir_entry(const pathname orig_name, entry_type type)
+    : m_orig_subpath(orig_name), m_subpath(orig_name.canonicalize()),
+      m_file_name(m_subpath.basename()), m_attr(), m_type(type) {}
+
+
+dir_entry::dir_entry(const lister::entry &ent) : dir_entry(ent.name, ent.type) {}
+
+dir_entry::dir_entry(const lister::entry &ent, const struct stat &st) : dir_entry(ent.name, ent.type) {
+    m_attr = st;
+}
+
+dir_entry::dir_entry(pathname path, const struct stat &st) : dir_entry(std::move(path), IFTODT(st.st_mode & S_IFMT)) {
+    m_attr = st;
+}
+
+
+const pathname &dir_entry::orig_subpath() const {
+    return m_orig_subpath;
+}
+void dir_entry::orig_subpath(nuc::pathname path) {
+    m_orig_subpath = path;
+    subpath(std::move(path).canonicalize());
+}
+
+const pathname &dir_entry::subpath() const {
+    return m_subpath;
+}
+void dir_entry::subpath(const pathname &path) {
+    m_subpath = path;
+    m_file_name = path.basename();
+}
+
+const pathname::string &dir_entry::file_name() const {
+    return m_file_name;
+}
+
+
+dir_entry::entry_type dir_entry::ent_type() const noexcept {
+    return m_type;
+}
+void dir_entry::ent_type(entry_type type) noexcept {
+    m_type = type;
+}
+void dir_entry::ent_type(uint8_t type) noexcept {
+    m_type = dt_to_entry_type(type);
+}
+
+
+dir_entry::entry_type dir_entry::type() const noexcept {
+    uint8_t type = IFTODT(m_attr.st_mode);
+
+    return type != DT_UNKNOWN ? dt_to_entry_type(type) : m_type;
+}
+
+dir_entry::entry_type dir_entry::dt_to_entry_type(uint8_t type) noexcept {
     switch (type) {
     case DT_UNKNOWN:
         return type_unknown;
@@ -54,40 +113,10 @@ dir_entry::entry_type dir_entry::dt_to_entry_type(uint8_t type) {
     return type_unknown;
 }
 
-// The default constructor call m_attr() is required to value
-// initialize all members of the stat struct to zero
 
-dir_entry::dir_entry(const pathname orig_name, uint8_t type) : dir_entry(orig_name, dt_to_entry_type(type)) {}
-
-dir_entry::dir_entry(const pathname orig_name, entry_type type)
-    : m_orig_subpath(orig_name), m_subpath(orig_name.canonicalize()),
-      m_file_name(m_subpath.basename()), m_attr(), m_type(type) {}
-
-
-dir_entry::dir_entry(const lister::entry &ent) : dir_entry(ent.name, ent.type) {}
-
-dir_entry::dir_entry(const lister::entry &ent, const struct stat &st) : dir_entry(ent.name, ent.type) {
+const struct stat &dir_entry::attr() const {
+    return m_attr;
+}
+void dir_entry::attr(const struct stat &st) {
     m_attr = st;
-}
-
-dir_entry::dir_entry(pathname path, const struct stat &st) : dir_entry(std::move(path), IFTODT(st.st_mode & S_IFMT)) {
-    m_attr = st;
-}
-
-
-dir_entry::entry_type dir_entry::ent_type() const {
-    return m_type;
-}
-void dir_entry::ent_type(entry_type type) {
-    m_type = type;
-}
-void dir_entry::ent_type(uint8_t type) {
-    m_type = dt_to_entry_type(type);
-}
-
-
-dir_entry::entry_type dir_entry::type() const {
-    uint8_t type = IFTODT(m_attr.st_mode);
-
-    return type != DT_UNKNOWN ? dt_to_entry_type(type) : m_type;
 }
--- src/directory/dir_entry.h
+++ src/directory/dir_entry.h
@@ -17,8 +17,8 @@
  *
  */
 
-#ifndef NUC_DIR_ENTRY_H
-#define NUC_DIR_ENTRY_H
+#ifndef NUC_DIRECTORY_DIR_ENTRY_H
+#define NUC_DIRECTORY_DIR_ENTRY_H
 
 #include "types.h"
 #include "lister/lister.h"
@@ -35,25 +35,6 @@ namespace nuc {
      * attributes.
      */
     class dir_entry {
-        /**
-         * Original non-canonicalized subpath of the entry.
-         */
-        pathname m_orig_subpath;
-        /**
-         * Canonicalized subpath.
-         */
-        pathname m_subpath;
-        /**
-         * The file name of the entry, i.e. the basename of the
-         * canonicalized subpath of the entry.
-         */
-        pathname::string m_file_name;
-
-        /**
-         * Stat attributes of the underlying file.
-         */
-        struct stat m_attr;
-
     public:
         /**
          * Entry type constants.
@@ -84,102 +65,136 @@ namespace nuc {
         /**
          * Constructs a 'dir_entry' object with a given name and type.
          *
-         * orig_subpath: The non-canonicalized subpath.
-         * type:         The type of the entry, as a dirent (DT_) constant.
+         * @param orig_subpath The non-canonicalized subpath.
+         * @param type The type of the entry, as a dirent (DT_) constant.
          */
         dir_entry(const pathname orig_subpath, uint8_t type);
         /**
          * Constructs a 'dir_entry' object with a given name and type.
          *
-         * orig_subpath: The non-canonicalized subpath.
-         * type:         The type of the entry, as an entry_type constant.
+         * @param orig_subpath The non-canonicalized subpath.
+         * @param type The type of the entry, as an entry_type constant.
          */
         dir_entry(const pathname orig_subpath, entry_type type);
 
         /**
-         * Constructs a 'dir_entry' object from a 'lister' object.
+         * Constructs a 'dir_entry' object from a 'lister::entry'
+         * object.
+         *
+         * @param ent The lister::entry object.
          */
         dir_entry(const lister::entry &ent);
 
         /**
-         * Constructs a 'dir_entry' object from 'lister::entry' object
+         * Constructs a 'dir_entry' object from a 'lister::entry' object
          * with given stat attributes.
+         *
+         * @param ent The lister::entry object.
+         * @param st The stat attributes
          */
         dir_entry(const lister::entry &ent, const struct stat &st);
 
+        /**
+         * Constructs a 'dir_entry' object with a given path and stat
+         * attributes.
+         *
+         * @param path The original pathname of the entry.
+         * @param st The stat attributes
+         */
         dir_entry(pathname path, const struct stat &st);
+
 
         /**
          * Returns the original non-canonicalized subpath.
+         *
+         * @return pathname
          */
-        const pathname &orig_subpath() const {
-            return m_orig_subpath;
-        }
+        const pathname &orig_subpath() const;
 
         /**
          * Changes the original sub-path of the entry and updates the
          * canonicalized subpath and file name.
+         *
+         * @param path The new path.
          */
-        void orig_subpath(pathname path) {
-            m_orig_subpath = path;
-            subpath(std::move(path).canonicalize());
-        }
+        void orig_subpath(pathname path);
+
 
         /**
          * Returns the canonicalized subpath.
+         *
+         * @return pathname
          */
-        const pathname &subpath() const {
-            return m_subpath;
-        }
-        /**
-         * Sets the canonicalized subpath, and file name.
-         */
-        void subpath(const pathname &path) {
-            m_subpath = path;
-            m_file_name = path.basename();
-        }
+        const pathname &subpath() const;
 
         /**
          * Returns the file name component of the canonicalized
          * subpath.
+         *
+         * @return string
          */
-        const pathname::string &file_name() const {
-            return m_file_name;
-        }
+        const pathname::string &file_name() const;
+
 
         /**
          * Returns the entry type.
+         *
+         * @return entry_type constant
          */
-        entry_type ent_type() const;
+        entry_type ent_type() const noexcept;
+
         /**
          * Sets the entry type. The stat mode attribute is not
          * changed.
+         *
+         * @param type The new entry type.
          */
-        void ent_type(entry_type type);
-        void ent_type(uint8_t type);
+        void ent_type(entry_type type) noexcept;
+        void ent_type(uint8_t type) noexcept;
 
         /**
          * Returns the type of the underlying file as an entry_type
          * constant. If the type of the underlying file is unknown,
          * the entry type is returned instead.
+         *
+         * @return entry_type constant.
          */
-        entry_type type() const;
+        entry_type type() const noexcept;
+
 
         /**
          * Returns the stat attributes.
+         *
+         * @return Reference to stat struct
          */
-        const struct stat &attr() const {
-            return m_attr;
-        }
+        const struct stat &attr() const;
 
         /**
          * Sets the stat attributes.
+         *
+         * @param st The new stat attribute struct.
          */
-        void attr(const struct stat &st) {
-            m_attr = st;
-        }
+        void attr(const struct stat &st);
 
     private:
+        /**
+         * Original non-canonicalized subpath of the entry.
+         */
+        pathname m_orig_subpath;
+        /**
+         * Canonicalized subpath.
+         */
+        pathname m_subpath;
+        /**
+         * The file name of the entry, i.e. the basename of the
+         * canonicalized subpath of the entry.
+         */
+        pathname::string m_file_name;
+
+        /**
+         * Stat attributes of the underlying file.
+         */
+        struct stat m_attr;
 
         /**
          * The type of the entry itself, not the underlying file.
@@ -197,11 +212,18 @@ namespace nuc {
          *
          * @return The entry_type constant.
          */
-        static entry_type dt_to_entry_type(uint8_t type);
+        static entry_type dt_to_entry_type(uint8_t type) noexcept;
+
+        /**
+         * Sets the canonicalized subpath, and file name.
+         *
+         * @param path The canonicalized path.
+         */
+        void subpath(const pathname &path);
     };
 }
 
-#endif // NUC_DIR_ENTRY_H
+#endif // NUC_DIRECTORY_DIR_ENTRY_H
 
 // Local Variables:
 // mode: c++
